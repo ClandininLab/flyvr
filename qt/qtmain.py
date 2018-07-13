933d58172de7d751a1da1e69b3d3a1fc829675c0
sgherbst <sgherbst@gmail.com>
2018-07-12 19:50:46 -0700

    work in progress

--- qt/qtmain.py
+++ qt/qtmain.py
@@ -1,12 +1,18 @@
 import sys
-from PyQt5.QtWidgets import QDialog, QApplication
+import types
+from PyQt5.QtWidgets import QApplication
 from PyQt5 import uic
+from PyQt5.QtCore import QBasicTimer
+from functools import partial
 
-def myfunc():
-    print('yo')
+def valuechg(ui, data):
+    ui.thresh_label.setText(str(data))
 
 app = QApplication(sys.argv)
+
 ui = uic.loadUi('main.ui')
-ui.pushButton.clicked.connect(myfunc)
 ui.show()
+ui.thresh_slider.valueChanged.connect(partial(valuechg, ui))
+ui.thresh_slider.setValue(99)
 sys.exit(app.exec_())
+
