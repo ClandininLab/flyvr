93f86c586e8b9d565bc7fd75cd81acf2e18abd06
Luke Brezovec <brezovec@stanford.edu>
2018-11-06 11:54:00 -0800

    work on gui - laying base logic
    created trial file to replace main. Put some main logic in gui. began to hook up gui buttons.

---
+++ flyvr/trial.py
@@ -0,0 +1,211 @@
+class TrialThread(Service):
+    def __init__(self, exp_dir, cam, dispenser, mrstim, opto, cnc, ui,
+                 loopTime=10e-3, fly_lost_timeout=1, fly_detected_timeout=1, auto_change_rate=None):
+        self.trial_count = itertools.count(1)
+        self.state = 'startup'
+        self.prev_state = 'startup'
+
+        self.cam = cam
+        self.dispenser = dispenser
+        self.mrstim = mrstim
+        self.opto = opto
+        self.cnc = cnc
+        self.ui = ui
+        self.tracker = None
+        self.timer_start = None
+
+        self.exp_dir = exp_dir
+        self.fly_lost_timeout = fly_lost_timeout
+        self.fly_detected_timeout = fly_detected_timeout
+        self.auto_change_rate = auto_change_rate
+
+        # set up access to the thread-ending signal
+        self.manualLock = Lock()
+        self._manualCmd = None
+
+        self.trialDirLock = Lock()
+        self._trial_dir = None
+
+        # create folder for data
+        if platform.system() == 'Windows':
+            topdir = r'F:\FlyVR'
+        elif platform.system() == 'Linux':
+            topdir = '/mnt/fly-data/FlyVR'
+        else:
+            raise Exception('Invalid platform.')
+
+        # create top-level experiment directory
+        folder = 'exp-'+strftime('%Y%m%d-%H%M%S')
+        exp_dir = os.path.join(topdir, folder)
+        os.makedirs(exp_dir)
+
+        # start logging to dispenser
+        #try:
+        #    self.dispenser.start_logging(self.exp_dir)
+        #except OSError:
+        #    print('Could not set up dispenser logging.')
+
+        # call constructor from parent
+        super().__init__(minTime=loopTime, maxTime=loopTime, iter_warn=False)
+
+    @property
+    def manualCmd(self):
+        with self.manualLock:
+            return self._manualCmd
+
+    def resetManual(self):
+        with self.manualLock:
+            self._manualCmd = None
+
+    def manual(self, *args):
+        with self.manualLock:
+            self._manualCmd = args
+
+    def stop(self):
+        super().stop()
+        self.tracker.stop()
+        self.cnc.stop()
+
+    @property
+    def trial_dir(self):
+        with self.trialDirLock:
+            return self._trial_dir
+
+    def _start_trial(self):
+        trial_num = next(self.trial_count)
+        print('Started trial ' + str(trial_num))
+        folder = 'trial-' + str(trial_num) + '-' + strftime('%Y%m%d-%H%M%S')
+        _trial_dir = os.path.join(self.exp_dir, folder)
+        os.makedirs(_trial_dir)
+
+        self.cnc.startLogging(os.path.join(_trial_dir, 'cnc.txt'))
+        self.cam.startLogging(os.path.join(_trial_dir, 'cam.txt'),os.path.join(_trial_dir, 'cam_compr.mkv'))
+        self.opto.startLogging(os.path.join(_trial_dir, 'opto.txt'))
+
+        self._trial_dir = _trial_dir
+        self.mrstim.nextStim(self._trial_dir)
+
+    def _stop_trial(self):
+        print('Stopped trial.')
+
+        self.cnc.stopLogging()
+        self.cam.stopLogging()
+        self.mrstim.stopStim(self._trial_dir)
+
+        self.tracker.stopTracking()
+
+    def loopBody(self):
+        #self.mrstim.updateStim(self._trial_dir)
+
+        if self.state == 'startup':
+            print('** startup **')
+
+            # Open connection to CNC rig
+            #cnc_home()
+            # Start tracker thread
+            self.tracker = TrackThread(cncThread=self.cnc, camThread=self.cam)
+            self.tracker.start()
+            #self.tracker.move_to_center()
+
+            # go to the manual control state
+            self.resetManual()
+            self.state = 'manual'
+            print('** manual **')
+        elif self.state == 'started':
+            if self.manualCmd is not None:
+                self.state = 'manual'
+                print('** manual **')
+            elif self.cam.flyData.flyPresent:
+                print('Fly possibly found...')
+                self.timer_start = time()
+                self.state = 'fly detected'
+                self.tracker.startTracking()
+        elif self.state == 'fly detected':
+            if self.manualCmd is not None:
+                self.tracker.stopTracking()
+                self.prev_state = 'fly detected'
+                self.state = 'manual'
+                print('** manual **')
+            elif (time() - self.timer_start) >= self.fly_detected_timeout:
+                print('Fly found!')
+                self.tracker.startTracking()
+                self._start_trial()
+                self.prev_state = 'fly detected'
+                self.state = 'run'
+                #print('** run **')
+            elif not self.cam.flyData.flyPresent:
+                print('Fly lost.')
+                self.timer_start = time()
+                self.prev_state = 'fly detected'
+                self.state = 'fly lost'
+                self.tracker.stopTracking()
+        elif self.state == 'run':
+            if self.manualCmd is not None:
+                self._stop_trial()
+                self.state = 'manual'
+                print('** manual **')
+            elif not self.cam.flyData.flyPresent:
+                print('Fly possibly lost...')
+                self.timer_start = time()
+                self.prev_state = 'run'
+                self.state = 'fly lost'
+        elif self.state == 'fly lost':
+            if self.manualCmd is not None:
+                self._stop_trial()
+                self.prev_state = 'fly lost'
+                self.state = 'manual'
+                print('** manual **')
+            elif self.cam.flyData.flyPresent:
+                print('Fly located again.')
+                self.timer_start = time()
+                self.tracker.startTracking()
+                if self.prev_state == 'run':
+                    self.state = 'run'
+                elif self.prev_state == 'fly detected':
+                    self.state = 'fly detected'
+            elif (time() - self.timer_start) >= self.fly_lost_timeout:
+                if self.prev_state == 'run':
+                    print('Fly is gone.')
+                    self._stop_trial()
+                self.tracker.move_to_center()
+                try:
+                    self.dispenser.release_fly()
+                except OSError:
+                    print('Please dispense fly (could not release it automatically)')
+                self.prev_state = 'fly lost'
+                self.state = 'started'
+        elif self.state == 'manual':
+            manualCmd = self.manualCmd
+
+            if manualCmd is None:
+                pass
+            elif manualCmd[0] == 'start':
+                try:
+                    self.dispenser.release_fly()
+                except OSError:
+                    print('Please dispense fly (could not release it automatically)')
+                self.state = 'started'
+                print('** started **')
+            elif manualCmd[0] == 'stop':
+                print('** manual: stop **')
+            elif manualCmd[0] == 'center':
+                print('** manual: center **')
+                self.tracker.move_to_center()
+            elif manualCmd[0] == 'nojog':
+                print('** manual: nojog **')
+                self.tracker.manualVelocity = None
+            elif manualCmd[0] == 'jog':
+                manualVelocity = ManualVelocity(velX=manualCmd[1], velY=manualCmd[2])
+                self.tracker.manualVelocity = manualVelocity
+            elif manualCmd[0] == 'release_fly':
+                try:
+                    self.dispenser.release_fly()
+                except OSError:
+                    print('Please dispense fly (could not release it automatically)')
+            else:
+                raise Exception('Invalid manual command.')
+
+            if (manualCmd is not None) and (manualCmd[0] != 'jog'):
+                self.resetManual()
+        else:
+            raise Exception('Invalid state.')
--- qt/qtmain.py
+++ qt/qtmain.py
@@ -7,30 +7,93 @@ import flyvr.gate_control
 from flyvr.opto import OptoThread
 from flyrpc.launch import launch_server
 
-def main():
-    def valuechg(ui, data):
+class MainGui():
+    def __init__(self):
+
+        app = QApplication(sys.argv)
+
+        self.ui = uic.loadUi('main.ui')
+
+        self.ui.show()
+
+        # Set services to none
+        self.dispenser = None
+        self.opto = None
+        self.cam = None
+        self.cnc = None
+        self.tracker = None
+        self.stim = None
+
+        self.ui.thresh_slider.valueChanged.connect(partial(valuechg, self.ui))
+        self.ui.thresh_slider.setValue(99)
+
+        # Setup cnc buttons
+        self.ui.cnc_start_button.clicked.connect(lambda x: cncStart())
+
+        # Setup cam buttons
+        self.ui.camera_start_button.clicked.connect(lambda x: camStart())
+
+        # Setup visual stimulus buttons
+        self.ui.stim_start_button.clicked.connect(lambda x: stimStart())
+
+        # Setup dispenser buttons
+        self.ui.dispenser_start_button.clicked.connect(lambda x: dispenserStart())
+        self.ui.dispenser_stop_button.clicked.connect(lambda x: dispenserStop())
+        self.ui.start_trial_button.clicked.connect(lambda x: self.dispenser.release_fly())
+        self.ui.open_gate_button.clicked.connect(lambda x: self.dispenser.open_gate())
+        self.ui.close_gate_button.clicked.connect(lambda x: self.dispenser.close_gate())
+
+        # Setup opto buttons
+        self.ui.opto_start_button.clicked.connect(lambda x: optoStart())
+        self.ui.opto_on_button.clicked.connect(lambda x: opto.on())
+        self.ui.opto_off_button.clicked.connect(lambda x: opto.off())
+        self.ui.opto_pulse_button.clicked.connect(lambda x: opto.pulse())
+
+        # Setup main experiment buttons
+        self.ui.start_experiment_button.clicked.connect(lambda x: experimentStart())
+
+        # TRY WITHOUT LAMBDAS AND WITHOUT ()
+
+    def valuechg(self, data):
         ui.thresh_label.setText(str(data))
 
-    app = QApplication(sys.argv)
+    def dispenserStart(self):
+        self.dispenser = launch_server(flyvr.gate_control)
+        self.ui.dispenser_start_button.setEnabled(False)
+        self.ui.dispenser_stop_button.setEnabled(True)
 
-    ui = uic.loadUi('main.ui')
+    def dispenserStop(self):
+        pass
 
-    ui.show()
+    def cncStart(self):
+        self.cnc = CncThread()
+        self.cnc.start()
 
-    ui.thresh_slider.valueChanged.connect(partial(valuechg, ui))
-    ui.thresh_slider.setValue(99)
+    def camStart(self):
+        self.cam = CamThread()
+        self.cam.start()
 
-    dispenser = launch_server(flyvr.gate_control)
-    ui.start_trial_button.clicked.connect(lambda x: dispenser.release_fly())
-    ui.open_gate_button.clicked.connect(lambda x: dispenser.open_gate())
-    ui.close_gate_button.clicked.connect(lambda x: dispenser.close_gate())
+    def stimStart(self):
+        self.stim = MrDisplay()
 
-    opto = OptoThread()
-    ui.opto_on_button.clicked.connect(lambda x: opto.on())
-    ui.opto_off_button.clicked.connect(lambda x: opto.off())
-    ui.opto_pulse_button.clicked.connect(lambda x: opto.pulse())
+    def optoStart(self):
+        self.opto = OptoThread(cncThread=self.cnc, camThread=self.cam,
+                               TrackThread=self.tracker)
+        self.opto.start()
+
+    def trackerStart(self):
+        self.tracker = TrackThread(cncThread=self.cnc, camThread=self.cam)
+        self.tracker.start()
+
+    def experimentStart(self):
+        self.trialThread = TrialThread(cam=self.cam, dispenser=self.dispenser,
+                                       mrstim=self.mrstim, opto=self.opto, stim=self.stim, ui=self.ui)
+        self.trialThread.start()
 
     sys.exit(app.exec_())
+
+def main():
+    MainGui()
 
 if __name__ == '__main__':
     main()
