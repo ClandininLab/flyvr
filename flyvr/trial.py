ea2672d8cf930afc3c7e6a4972a71d10b3828dfa
Steven Herbst <brezovec@stanford.edu>
2020-07-17 14:34:02 -0700

    added fix if dispenser is not on to work with new dispenser fix

--- flyvr/trial.py
+++ flyvr/trial.py
@@ -207,9 +207,10 @@ class TrialThread(Service):
                 self.tracker.startTracking()
                 ## add here to update a dispenser state based on a false close or fly stuck in tunnel state
                 #self.dispenser.no_fly_reopen_gate = False
-                self.dispenser.prev_state = 'Idle' #this prevents it from retriggering the gate open state
-                self.dispenser.state = 'Idle'
-                print('REOPEN GATE SET TO FALSE--state set to idle')
+                if self.dispenser is not None:
+                    self.dispenser.prev_state = 'Idle' #this prevents it from retriggering the gate open state
+                    self.dispenser.state = 'Idle'
+                    print('REOPEN GATE SET TO FALSE--state set to idle')
         elif self.state == 'fly detected':
             if (time() - self.timer_start) >= self.fly_detected_timeout:
                 print('Fly found!')
