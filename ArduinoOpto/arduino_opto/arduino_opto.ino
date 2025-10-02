5307644fd4db68c4db90480e0a053d328bd8a377
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2025-10-02 12:10:46 -0700

    trying one pin arudino

--- ArduinoOpto/arduino_opto/arduino_opto.ino
+++ ArduinoOpto/arduino_opto/arduino_opto.ino
@@ -1,20 +1,20 @@
-#define LED_PIN 8
+//#define LED_PIN 8
 #define LED_PIN_LASER 9 
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
+  //pinMode(LED_PIN, OUTPUT);
   pinMode(LED_PIN_LASER, OUTPUT);
   led_off();
   Serial.begin(9600); 
 }
 
 void led_on(){
-  digitalWrite(LED_PIN, LOW);
+  //digitalWrite(LED_PIN, LOW);
   digitalWrite(LED_PIN_LASER, HIGH);
 }
 
 void led_off(){
-  digitalWrite(LED_PIN, HIGH);
+  //digitalWrite(LED_PIN, HIGH);
   digitalWrite(LED_PIN_LASER, LOW);
 }
 
